069b2f9d4d40c1401910e5dde29ebc4e3ef1480d
Fazail Alam <80394548+mfazail@users.noreply.github.com>
2022-12-05 18:11:15 +0000

    dsa update-2

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,26 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
 	extends: 'content-wind',
+	modules: [
+		'@kevinmarrec/nuxt-pwa'
+	],
 	content: {
 		highlight: {
 			preload: ['java','c']
+		}
+	},
+	pwa: {
+		manifest: {
+			name: "College Prep",
+			description: "Semester Exam Preparation notes",
+			short_name:"College Prep"
+		},
+		meta: {
+			author: "Fazail Alam",
+			description: "Semester Exam Preparation notes",
+			name: "College Prep",
+			theme_color: "#6366f1",
+			title: "College Prep",
 		}
 	}
 })
